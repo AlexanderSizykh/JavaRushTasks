163c9f593ed3f0fbb4e99d3e014f90933ccdb949
AlexanderSizykh <alexsneezy@gmail.com>
2024-12-12 16:34:26 +0300

    tasks 1020-1101

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -1,5 +1,14 @@
 package com.javarush.task.pro.task10.task1020;
 
 public class Computer {
-
+    private Keyboard keyboard;
+    private Monitor monitor;
+    private Mouse mouse;
+    private SystemUnit systemUnit;
+    public Computer() {
+        keyboard = new Keyboard();
+        monitor = new Monitor();
+        mouse = new Mouse();
+        systemUnit = new SystemUnit();
+    }
 }
